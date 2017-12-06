7ed8b17021864813cd4e048f423d035c98d88f91
Zeeshan Nawaz <zeeshan.nawaz@insight-centre.org>
2017-12-06 15:20:43 +0000

    Reuters business news parsing - basic

--- crawler.py
+++ crawler.py
@@ -1,6 +1,12 @@
-from abc import ABCMeta, abstractmethod
-import scrapy
-from scrapy.linkextractors import LinkExtractor
+from abc import ABCMeta,abstractmethod
+import requests, re
+from bs4 import BeautifulSoup
+import pdb
+from urllib3.exceptions import MaxRetryError
+import re
+import nltk
+import os
+from nltk.stem.snowball import SnowballStemmer
 
 
 class CrawlerSpider(object):
@@ -14,28 +20,51 @@ class CrawlerSpider(object):
     def get_news_info(self):
         NotImplemented("Function not implemented!")
 
+    @abstractmethod
+    def parse_page(self):
+        NotImplemented("Function not implemented")
+
 
 
 
 class ReutersCrawlerSpider(CrawlerSpider):
 
-    def __init__(self):
-        self.domain_url = 'www.reuters.com'
-        self.starts_url = "http://www.reuters.com/news/archive/businessNews?page=1&pageSize=10&view=page"
-        self.rules = LinkExtractor(allow=r'\?page=[0-9]&pageSize=10&view=page', restrict_xpaths=('//div[@class="pageNavigation"]'),
-                                                        callback='parse_item', follow=True)
+    def __init__(self, language = "English", start_page = 1, page_size = 10):
+        self.domain_url = 'http://www.reuters.com'
+        self.language = language
+        self.business_news_url = os.path.join(self.domain_url,"news","archive","businessNews")
+        self.starts_url = os.path.join(self.business_news_url,"?page=1&pageSize=10&view=page")
+        self.headers = {'User-agent' : 'Mozilla/11.0'}
+        self.params = {'page' : start_page , 'pageSize' : page_size, 'view' : 'page'}
 
-        def parse_item(self, response):
-            questions = response.xpath('//div[@class="summary"]/h3')
 
-            for question in questions:
-                item = StackItem()
-                item['url'] = question.xpath(
-                    'a[@class="question-hyperlink"]/@href').extract()[0]
-                item['title'] = question.xpath(
-                    'a[@class="question-hyperlink"]/text()').extract()[0]
-                yield item
 
+
+    def parse_page(self, country_name = None):
+        response = requests.get(self.starts_url,params=self.params, headers = self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        print(soup)
+        h3tags = soup.find_all('a',class_='nav-link-subsec')
+        for h3 in h3tags:
+            if country_name:
+                if re.search(country_name,str(h3['href'])):
+                    print(h3)
+            else:
+                print(h3)
+            #text = soup.get_text(separator=' ')
+            #text = re.sub("[^a-zA-Z]+", " ", text);
+            #print(text)
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    obj = ReutersCrawlerSpider()
+    obj.parse()
 
 
 
